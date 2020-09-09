76e6ea9a9fd2692081c502ba8f2a4fdb70ed0414
Leandro Magalhaes <leandro.magalhaes@theiconic.com.au>
2020-09-09 13:57:01 +1000

    Run migration with db-init

--- pandora/bin/docker-init.sh
+++ pandora/bin/docker-init.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
+echo "###############################################"
+echo "Waiting for MySQL to start"
+echo "###############################################"
 until  mysql -h "${DB_HOST}" -u "${DB_USERNAME}" -P "${DB_PORT}" -p"${DB_PASSWORD}" "${DB_NAME}" -e '\q'; do
   >&2 echo "Waiting for MySQL to start"
   sleep 5
 done
-
+echo "###############################################"
 echo "MySQL is up - executing command"
+echo "###############################################"
 
-env
-
-echo "Creating initial tables"
+echo ""
+echo "###############################################"
+echo "Running migrations"
+echo "###############################################"
 alembic upgrade head
+echo "###############################################"
+echo "Migrations finished"
+echo "###############################################"
+echo ""
